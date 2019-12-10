a990b5b603c76c831dbccbf338232ad8b308043c
Jhuan Marco Zamprogna <jhuanmarco@gmail.com>
2019-12-09 22:09:48 -0300

    Adicionado Pokebola
    Criado ambiente para o pokemon

--- cg.js
+++ cg.js
@@ -12,7 +12,7 @@ function createScene() {
     
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xFFFFFF );
+    scene.background = new THREE.Color( 0x888888 );
                 
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
     camera.translateX += 0;
@@ -28,18 +28,53 @@ function createScene() {
     
     camera.position.z = 10;
    
+   
+
+   
+    
+
+    var light = new THREE.AmbientLight( 0xffffff ); // soft white light
+    scene.add( light );
+    
+
+
+    var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+    scene.add( light );
+
+    
+
+    var light = new THREE.DirectionalLight( 0xffffff, 1, 1 );
+    light.position.set( 0, -20, 0 ); 			//default; light shining from top
+    light.castShadow = true;            // default false
+    scene.add( light );
+
+
+    //var geometry1 = new THREE.SphereGeometry(0.95,50,50);
+    var geometry1 = new THREE.SphereBufferGeometry(2.99, 50, 50, 0, 2*Math.PI, 0, 0.5 * Math.PI);
+    var material1 = new THREE.MeshLambertMaterial({color: 0xffffff, transparent: true, opacity: 0.1});
+    var sphere1 = new THREE.Mesh(geometry1, material1);
+    sphere1.position.y += 2.3;
+    sphere1.position.x -= 0.97;
+    sphere1.position.z -= 0.4;
+    
+    scene.add(sphere1);
+        
+
 
     controls = new OrbitControls( camera, renderer.domElement );
   
     var loader = new GLTFLoader();
-    loader.load( './Models/zubat/scene.gltf', function ( gltf ) {
+    loader.load( './Models/pokeball/scene.gltf', function ( gltf ) {
         model = gltf.scene;
         scene.add( model );
-        model.scale.set(0.01, 0.01, 0.01);
+        model.scale.set(1,1,1);
+        
     }, undefined, function ( e ) {
         console.error( e );
         } );
        
+    renderer.gammaOutput = true;
+    renderer.gammaFactor = 2.2;
 
 }
 
