775c348a3e3a19b48860532d23e81f26f429fef3
Jhuan Marco Zamprogna <jhuanmarco@gmail.com>
2019-12-09 21:07:34 -0300

    Adicionando Modelo
    Não carregou textura, necessária revisão

--- cg.js
+++ cg.js
@@ -4,31 +4,46 @@ import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threej
 import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://threejsfundamentals.org/threejs/resources/threejs/r110/examples/jsm/loaders/GLTFLoader.js';
 
-var camera, scene, renderer, controls;
+var camera, scene, renderer, model, cube, face, controls;
 
 // Inicial 
 
-function createScena() {
+function createScene() {
+    
 
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    scene.background = new THREE.Color( 0xFFFFFF );
+                
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera.translateX += 0;
     
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
- 
+   /*
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 5;
+    scene.add( cube ); */ 
+    
+    camera.position.z = 10;
+   
 
     controls = new OrbitControls( camera, renderer.domElement );
+  
+    var loader = new GLTFLoader();
+    loader.load( './Models/zubat/scene.gltf', function ( gltf ) {
+        model = gltf.scene;
+        scene.add( model );
+        model.scale.set(0.01, 0.01, 0.01);
+    }, undefined, function ( e ) {
+        console.error( e );
+        } );
+       
 
 }
 
-createScena();
+createScene();
 
 
 
